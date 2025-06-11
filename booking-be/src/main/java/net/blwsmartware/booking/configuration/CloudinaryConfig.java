9ed9e09824c9279c87464df9205f03be1acd31dd
vuchungbt <dinhmenhbt096@gmail.com>
2025-06-11 12:37:10 +0700

    done cloud image

--- booking-be/src/main/java/net/blwsmartware/booking/configuration/CloudinaryConfig.java
+++ booking-be/src/main/java/net/blwsmartware/booking/configuration/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package net.blwsmartware.booking.configuration;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,12 +23,10 @@ public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", cloudName);
-        config.put("api_key", apiKey);
-        config.put("api_secret", apiSecret);
-        config.put("secure", "true");
-
-        return new Cloudinary(config);
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        ));
     }
-} 
+}
